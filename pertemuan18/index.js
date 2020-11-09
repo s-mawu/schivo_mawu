63ca28bf5a4c423e077c7206d5f7b0d57abfd3cc
s-mawu <69877095+s-mawu@users.noreply.github.com>
2020-11-09 22:34:31 +0800

    Add files via upload

---
+++ pertemuan18/index.js
@@ -0,0 +1,48 @@
+// console.log("Hello JavaScript");
+//Variable Let and Const:
+// let $umur = 25;
+// let $nilai = 95.5;
+// let $nama = "John Doe";
+// let sudahMenikah = true;
+// console.log($umur);
+// console.log($nama);
+// console.log($nilai);
+
+// let alamat;
+// console.log(alamat);
+
+// const tahunLahir = 2010;
+// console.log(tahunLahir);
+
+//Operator Aritmatik:
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1 + angka2);
+
+// let umur = 32;
+// let namaDepan = "John";
+// let namaBelakang = "Doe";
+// console.log(
+//     "Nama Saya " +  
+//     namaDepan + 
+//     " " + 
+//     namaBelakang +
+//     ". Umur saya adalah " +
+//     umur +  " tahun" 
+// );
+
+//Operator Perbandingan:
+let angka1 = 10;
+let angka2 = 9;
+console.log(angka1 <= angka2);
+
+//Ternary Operator:
+let angka = 5;
+let hasil = angka <= 0? "Negatif" : "Positif";
+console.log(hasil);
+
+//Logic Operator:
+let kondisi1 = true;
+let kondisi2 = true;
+console.log(kondisi1 || kondisi2);
+console.log(!kondisi1);
